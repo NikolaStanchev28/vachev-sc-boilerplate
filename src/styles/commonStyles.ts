5380a909627c2be7c70769f134174c3ae21f8bc3
NikolaStanchev28 <105424133+NikolaStanchev28@users.noreply.github.com>
2023-09-21 23:20:50 +0300

    improve typography theme boilerplate

--- src/styles/commonStyles.ts
+++ src/styles/commonStyles.ts
@@ -1,37 +1,53 @@
-import { css, FlattenSimpleInterpolation } from "styled-components";
+import { css } from "styled-components";
+
+type CSS = ReturnType<typeof css>;
 
 export interface TypographyStylesProps {
-  h1: { regular: FlattenSimpleInterpolation };
-  h2: { regular: FlattenSimpleInterpolation };
-  h3: { regular: FlattenSimpleInterpolation };
-  h4: { regular: FlattenSimpleInterpolation };
-  h5: { regular: FlattenSimpleInterpolation };
-  h6: { regular: FlattenSimpleInterpolation };
+  h1: { regular: CSS };
+  h2: { regular: CSS };
+  h3: { regular: CSS };
+  h4: { regular: CSS };
+  h5: { regular: CSS };
+  h6: { regular: CSS };
   p: {
-    regular: FlattenSimpleInterpolation;
+    regular: CSS;
   };
 }
 
 export const typography: TypographyStylesProps = {
   h1: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   },
   h2: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   },
   h3: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   },
   h4: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   },
   h5: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   },
   h6: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   },
   p: {
-    regular: css``
+    regular: css`
+      ${({ theme: { colors, breakpoint } }) => css``}
+    `
   }
 };
